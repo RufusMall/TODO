436a94a131108363b1d39f5cc4dd0e8f11f5c91f
Rufus <Rufusmall@hotmail.com>
2020-04-17 18:34:42 +0100

    Add workspace with basic project, including realm dependancy.
    Squashed commit of the following:
    
    commit 302fda42b94c7eea37f9649a9778c58c0c8f6aae
    Author: Rufus <Rufusmall@hotmail.com>
    Date:   Fri Apr 17 18:27:47 2020 +0100
    
        build test
    
    commit 9e3fd4a4fa5e542472a1bc12b8bfb9663d9829f4
    Author: Rufus <Rufusmall@hotmail.com>
    Date:   Fri Apr 17 18:09:31 2020 +0100
    
        build test
    
    commit f063ee26261b5c728331945e99cf67d647cebd45
    Author: Rufus <Rufusmall@hotmail.com>
    Date:   Fri Apr 17 18:06:31 2020 +0100
    
        build test
    
    commit 857491b718e660e84aa9ff6793be724ef5d86273
    Author: Rufus <Rufusmall@hotmail.com>
    Date:   Fri Apr 17 17:58:38 2020 +0100
    
        Add build status to readme
    
    commit 7692fe19b999408c6fded1348d43988327961575
    Author: Rufus <Rufusmall@hotmail.com>
    Date:   Fri Apr 17 17:55:57 2020 +0100
    
        Add realm pod
    
    commit 24a13adbaf1ec5c5546abca6d8253b069b42ae80
    Author: Rufus <Rufusmall@hotmail.com>
    Date:   Fri Apr 17 17:33:53 2020 +0100
    
        build test
    
    commit e96b95bd52d66caa9540b9b398d9ccddabd0d8ac
    Author: Rufus <Rufusmall@hotmail.com>
    Date:   Fri Apr 17 17:27:19 2020 +0100
    
        build test
    
    commit cf24e8b8561d37cbb15980af37ff5e5fcdd64307
    Author: Rufus <Rufusmall@hotmail.com>
    Date:   Fri Apr 17 17:20:35 2020 +0100
    
        Build trigger - empty commit
    
    commit 62280cf7ae8a74d8e480cb1d82355464173c8219
    Author: Rufus <Rufusmall@hotmail.com>
    Date:   Fri Apr 17 16:50:32 2020 +0100
    
        update schemes
    
    commit 20c18ae5a500a5874e425c57ed0e5c2dfd61fa73
    Author: Rufus <Rufusmall@hotmail.com>
    Date:   Fri Apr 17 16:38:09 2020 +0100
    
        Inital commit - includes basic template project and readme

---
+++ Todo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Todo
+//
+//  Created by Rufus on 17/04/2020.
+//  Copyright © 2020 Rufus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
