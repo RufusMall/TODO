20c18ae5a500a5874e425c57ed0e5c2dfd61fa73
Rufus <Rufusmall@hotmail.com>
2020-04-17 16:38:09 +0100

    Inital commit - includes basic template project and readme

---
+++ Todo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Todo
+//
+//  Created by Rufus on 17/04/2020.
+//  Copyright © 2020 Rufus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Todo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Todo
+//
+//  Created by Rufus on 17/04/2020.
+//  Copyright © 2020 Rufus. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
